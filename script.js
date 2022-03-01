addafe83860551fe80beaa44c7a50e1f4c32ef8a
frank <kuete.frank1@gmail.com>
2022-03-01 17:42:05 +0100

    few updates

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(scrollY > 20){
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (scrollY > 20) {
             $('.navbar').addClass('sticky');
         }
-        else{
+        else {
             $('.navbar').removeClass('sticky');
         }
+        if (scrollY > 500) {
+            $('.scroll-up-btn').addClass('show');
+        }
+        else {
+            $('.scroll-up-btn').removeClass('show');
+        }
     })
-    $('.menu-btn').click(function(){
+
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
+
+    })
+
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass('active')
         $('.navbar .menu-btn i').toggleClass('active')
     })
