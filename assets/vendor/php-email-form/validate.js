80af99528fddd68cd987b0ba5cffae5515b1d723
frank <kuete.frank1@gmail.com>
2023-01-21 12:25:57 +0100

    adding mailing service

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -3,6 +3,8 @@
 * URL: https://bootstrapmade.com/php-email-form/
 * Author: BootstrapMade.com
 */
+
+
 (function () {
   "use strict";
 
@@ -14,59 +16,41 @@
 
       let thisForm = this;
 
-      let action = thisForm.getAttribute('action');
-      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
-      
-      if( ! action ) {
-        displayError(thisForm, 'The form action property is not set!')
-        return;
-      }
+      var email = document.getElementById('email').value
+      var subject = document.getElementById('subject').value
+      var name = document.getElementById('name').value
+      var message = document.getElementById('message').value
+
+      thisForm.reset();
+
       thisForm.querySelector('.loading').classList.add('d-block');
       thisForm.querySelector('.error-message').classList.remove('d-block');
       thisForm.querySelector('.sent-message').classList.remove('d-block');
 
-      let formData = new FormData( thisForm );
-
-      if ( recaptcha ) {
-        if(typeof grecaptcha !== "undefined" ) {
-          grecaptcha.ready(function() {
-            try {
-              grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
-              .then(token => {
-                formData.set('recaptcha-response', token);
-                php_email_form_submit(thisForm, action, formData);
-              })
-            } catch(error) {
-              displayError(thisForm, error)
-            }
-          });
-        } else {
-          displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
-        }
-      } else {
-        php_email_form_submit(thisForm, action, formData);
-      }
+      email_form_submit(thisForm , email , subject , name , message);
     });
   });
 
-  function php_email_form_submit(thisForm, action, formData) {
-    fetch(action, {
-      method: 'POST',
-      body: formData,
-      headers: {'X-Requested-With': 'XMLHttpRequest'}
+
+
+  function email_form_submit(thisForm, email , subject , name , message){
+    Email.send({
+    SecureToken : "827c4cd3-0ea5-4faa-8513-03c2c0ed6c16",
+    To : email,
+    From : "kuete.frank1@gmail.com",
+    Subject :subject,
+    Body : 'Name of the sender  : ' + name +'<br/> Messsage content : ' + message 
     })
-    .then(response => {
-      return response.text();
-    })
-    .then(data => {
-      thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
-        thisForm.querySelector('.sent-message').classList.add('d-block');
-        thisForm.reset(); 
-      } else {
-        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
+    .then(message =>{
+        thisForm.querySelector('.loading').classList.remove('d-block');
+        if (!message){
+        throw new Error('Form submission failed and no error message returned from' ); 
+        }
+        else{
+          alert('Message have been Sent' + message);
+        }
       }
-    })
+    )
     .catch((error) => {
       displayError(thisForm, error);
     });
